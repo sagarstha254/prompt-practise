3d4a5d2d3436beed4567387e0d0fcd7eb636a408
sagarstha254 <sagarstha254@gmail.com>
2022-01-13 12:33:22 +0545

    prompt practise

---
+++ indo.js
@@ -0,0 +1,11 @@
+console.log("hello");
+let name = prompt("enter first name");
+if (name === "sagar"){
+    alert("WELCOME");
+}
+else if (name ==="ocean"){
+    alert("hii");
+}
+else{
+    console.log("idiot");
+}
